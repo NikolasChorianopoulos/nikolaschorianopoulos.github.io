58960cf30c81a111170f6fc725035d7b2ef93c83
Nikolas Chorianopoulos <chorianopoulosnikolas@gmail.com>
2025-09-22 20:12:27 +0300

    aaa

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import SocialButton from "./SocialButton";
 
 const Hero = () => {
   return (
     <div className="flex flex items-center justify-evenly">
-      <div className="flex flex-col items-center justify-center min-h-100 ">
-        <h1 className="text-4xl font-bold">Sup, I'm Nikolas Chorianopoulos</h1>
-        <h2 className="text-2xl">And this is my life.</h2>
-        <SocialButton />
-      </div>
+      <div className="flex flex-col items-center justify-center min-h-100 "></div>
       <div>
-        <img src="/Me.jpg" alt="Me" className="w-96 h-96 rounded-lg" />
+        <img src="/sMe.jpg" alt="Me" className="w-96 h-96 rounded-lg" />
       </div>
     </div>
   );
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Navbar from "./Navbar";
+import Socials from "./Socials";
 
 const Layout = ({ children }) => {
   return (
     <>
       <main className="px-4 py-8 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] min-h-screen">
-        <Navbar />
+        <Socials />
         {children}
       </main>
     </>
